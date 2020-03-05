137cf1fe9c41a9ccc4cac10c226726f4b79d8fdf
Code-Wizard91 <alimizan@hotmail.com>
2020-03-05 16:38:00 +0000

    added private URI

--- application/__init__.py
+++ application/__init__.py
@@ -4,12 +4,19 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fee323f72238c94ce34302364eb0b21a'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ali:password@localhost/flaskapp'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
+#app.config['SECRET_KEY'] = 'fee323f72238c94ce34302364eb0b21a'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ali:password@localhost/flaskapp'
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+bcrypt = Bcrypt(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+db = SQLAlchemy(app)
 
 from application import routes
