258b35a6d2d7f672730b9dd9b73e1f4d081ad21e
Code-Wizard91 <alimizan@hotmail.com>
2020-03-05 16:41:26 +0000

    added private keys

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'fee323f72238c94ce34302364eb0b21a'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ali:password@localhost/flaskapp'
+
+
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
