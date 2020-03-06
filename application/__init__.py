e98be38831834815a94812e34ca172cbc9896d87
Code-Wizard91 <alimizan@hotmail.com>
2020-03-06 10:55:16 +0000

    Added extra image functionality

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-
-
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
