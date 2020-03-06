42fc50ab7e77a356feab57919074e74b0dffb5fa
Code-Wizard91 <alimizan@hotmail.com>
2020-03-06 03:06:13 +0000

    added posts functionality

--- application/routes.py
+++ application/routes.py
@@ -7,39 +7,13 @@ from flask import render_template, url_for, flash, redirect, request
 from application.forms import RegistrationForm, LoginForm, UpdateAccountForm, AdvertForm
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-	{
-	'user' : 'Mizan',
-	'datecreated' : '22/02/2020',
-	'imageurl': 'https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/496634680.jpg?itok=C0PDTs1E',
-	'title': 'Mitsubishi Evo 9 MR360',
-	'cardescription' : 'Wynford Specialist Motors Have once again managed to get there hands on a stunning unmarked example of a Mitsubishi Lancer Evo FQ-300 Finished in Pamla red ,only 1 owner from new With only 11000 miles from new ,never seen the rain ,totally genuine just like when it left the factory,a real credit to its owner keeping it locked away indoors for Its 16 years ,This model is a genuine FQ-300 model with a FQ-330 upgrade from factory , also has the upgraded half leather recaro red & black Leather Seats,original bill of sale to hand, been serviced by Mitsubishi main dealers, Trackstar Tracker Fitted along with Clifford alarm ,2 Keys to hand, you will not find a better condition evo',
-	'mileage' : '200,000 miles',
-	'location' : 'Manchester, Salford',
-	'contact' : '07462516278',
-	'price' : '£15,000'
-
-	},
-	{
-	'user' : 'James Grant',
-	'imageurl':'https://www.classicdriver.com/sites/default/files/styles/full_width_slider/public/article_images/porsche-turbo-13.jpg',
-	'datecreated' : '2/02/2020',
-	'title': 'Rare 911 Turbo',
-	'cardescription' : '3 owners / 9985 miles / Full service history and 2 years Porsche Approved warranty / 3.8L - 560hp / White with Black leather interior / A vehicle which bears our Porsche Approved seal has been professionally prepared by our Porsche-trained Technicians using only Porsche Genuine Parts. Our commitment includes: Minimum 24 months Porsche Approved Warranty and Porsche Assistance* / Vehicle inspected in compliance with our 111-point checklist / All work performed by Porsche-trained Technicians using Porsche Genuine Parts / Routine servicing/maintenance due within three months or 3,000 miles carried out / Minimum of 12 months MOT** / Fitment of N-rated tyres to minimum tread depth of 3mm / Body refinishing to Porsche standards / Full valet and final quality inspection / Independently monitored and audited standards.',
-	'mileage' : '20,000 miles',
-	'location' : 'London, Ilford',
-	'contact' : '07462222278',
-	'price' : '£75,000'
-
-	}
-
-]
 
 
 @app.route("/home")
 @app.route("/")
 def home():
-	return render_template('home.html', posts = posts, title = 'Home')
+        posts = Adverts.query.all()
+        return render_template('home.html',posts = posts, title = 'Home')
 
 @app.route("/about")
 def about():
@@ -138,11 +112,11 @@ def new_advert():
     if form.validate_on_submit():
         if form.image.data:
             image_file = save_car_image(form.image.data)
-            advert = Adverts(user_id=1,car_title=form.title.data, car_descr=form.car_descr.data, price=form.price.data, mileage = form.mileage.data, location = form.location.data, contact_no=form.contact_no.data, image = image_file)
+            advert = Adverts(user_id=current_user.user_id,car_title=form.title.data, car_descr=form.car_descr.data, price=form.price.data, mileage = form.mileage.data, location = form.location.data, contact_no=form.contact_no.data, image = image_file)
             db.session.add(advert)
             db.session.commit()
             flash('Your Advert Has Been Created! ','success')
             return redirect(url_for('home'))
         else:
             flash('Sorry there was an error please try again','danger')
-    return render_template('create_advert.html', title='New Advert', form=form)
+    return render_template('create_advert.html', title='New Advert',form=form)
