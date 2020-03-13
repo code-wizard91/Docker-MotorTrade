b60d28f83468a1a5f2e9e1a8c5514a083a273f22
code-wizard91 <alimizan@hotmail.com>
2020-03-13 09:29:51 -0400

    added yaml

--- performance_db/Create.sql
+++ performance_db/Create.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE flaskapp:
+CREATE DATABASE IF NOT EXISTS flaskapp;
 CREATE TABLE IF NOT EXISTS `Users` (
 	`user_id` int(10) NOT NULL auto_increment,
 	`username` varchar(255) NOT NULL,
