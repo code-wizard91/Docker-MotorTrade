803d89a2ade62063cf4583ddead58bb9b4660146
Mahboob Ali <alimizan@hotmail.com>
2020-04-16 02:50:34 +0100

    Update Create.sql

--- performance_db/Create.sql
+++ performance_db/Create.sql
@@ -2,13 +2,4 @@ CREATE DATABASE flaskapp;
 
 USE flaskapp;
 
-CREATE TABLE users(
-  user_id int NOT NULL AUTO_INCREMENT, 
-  PRIMARY KEY (user_id)
-  );
-CREATE TABLE adverts(
-  adv_id int NOT NULL AUTO_INCREMENT, 
-  user_id int NOT NULL, 
-  PRIMARY KEY (adv_id),
-  FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+CREATE TABLE mytable (myfield VARCHAR(20));
